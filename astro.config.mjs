dc97e60c632ced4cb148053eece18391de35b52a
Raider-Laptop <mr3003gean@gmail.com>
2025-10-03 02:37:54 -0600

    Agregar soporte para sitemap: configurar el plugin en astro.config.mjs y crear robots.txt.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   image: {
     service: {
       entrypoint: "astro/assets/services/noop",
     },
   },
+
+  site: 'https://cinerama-seven.vercel.app',
+  integrations: [sitemap()],
 });
